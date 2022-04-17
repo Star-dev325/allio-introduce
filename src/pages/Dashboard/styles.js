335c492f022f25f7e47585b291e728e3ec15742b
Star <leonwang325@gmail.com>
2022-04-16 19:10:39 -0700

    Initialize.
    Set initialize.

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Board = styled.div`
+    width: 100%;
+    height: 100vh;
+`
---
+++ src/pages/Sign/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const userInterface =
+  <>
+
+  </>
+
+const Sign = () => {
+
+}
+
+export default Sign
